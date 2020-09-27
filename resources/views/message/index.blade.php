6d05cf4753c5d110233491a0658a96d822694c62
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-28 05:23:37 +0900

    Update index.blade.php

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -35,27 +35,27 @@
 
   <!-- Masthead -->
   <header class="masthead text-white text-center">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-9 mx-auto">
-          <h1 class="mb-5">Build a landing page for your business or project and generate more leads!</h1>
-        </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <form>
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
-              </div>
-              <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </header>
+   <div class="overlay"></div>
+   <div class="container">
+     <div class="row">
+       <div class="col-xl-9 mx-auto">
+         <h1 class="mb-5">株式会社○○のサイトにようこそ</h1>
+       </div>
+       <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+         <form>
+           <div class="form-row">
+             <div class="col-12 col-md-9 mb-2 mb-md-0">
+               <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
+             </div>
+             <div class="col-12 col-md-3">
+               <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
+             </div>
+           </div>
+         </form>
+       </div>
+     </div>
+   </div>
+ </header>
 
   <!-- Icons Grid -->
   <section class="features-icons bg-light text-center">
@@ -176,55 +176,23 @@
 
   <!-- Footer -->
   <footer class="footer bg-light">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
-          <ul class="list-inline mb-2">
-            <li class="list-inline-item">
-              <a href="#">About</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Contact</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Terms of Use</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Privacy Policy</a>
-            </li>
-          </ul>
-          <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2020. All Rights Reserved.</p>
-        </div>
-        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
-          <ul class="list-inline mb-0">
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-facebook fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-twitter-square fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-instagram fa-2x fa-fw"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+         <ul class="list-inline mb-2">
+           <li class="list-inline-item">
+             <a href="{{ url('/about') }}">会社概要</a>
+           </li>
+         </ul>
+         <p class="text-muted small mb-4 mb-lg-0">&copy; 2020 株式会社○○. All Rights Reserved.</p>
+       </div>
+     </div>
+   </div>
+ </footer>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <!-- Bootstrap core JavaScript -->
+ <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+ <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
 </body>
-
-</html>
+</html>​
