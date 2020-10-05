66318ae2800514e4cc6f74330943c965012f77ae
fbfb6123 <fbfb6123@gmail.com>
2020-10-05 14:29:25 +0900

    SalesTableSeeder

---
+++ database/migrations/2020_10_05_051222_create_sales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('company_name')->comment('会社名');
+            $table->integer('amount')->comment('販売金額');
+            $table->integer('year')->comment('販売した年');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
